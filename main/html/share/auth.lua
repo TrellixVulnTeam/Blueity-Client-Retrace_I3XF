d42a927a85226d73da66123922d9ea11cc20ac3d
Blue'ity <65516039+gagominecraft12@users.noreply.github.com>
2022-01-07 15:40:21 -0600

    another update o_o

--- main/html/share/auth.lua
+++ main/html/share/auth.lua
@@ -1,5 +1,7 @@
 local https = require("socket.http")
-local resp = {}
+local resp = {
+    get: AnyV
+}
 local git = https.request{
     url = 'https://api.github.com/';
 }
@@ -10,7 +12,10 @@ function value()
             arg('http://api.vyond.com');
     }
 end
+
+os.getenv("../../env.json");
+os.clock(math.abs( 1*12, randombut )).math.random( 1[, n] );
 -- When script is handled. will be entoxiced.
 
-local result = os.capture("echo hallo")
+local result = print(os.execute(os.date(*)));
 print(result)
